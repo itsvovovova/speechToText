6aebdbd93d64aa5be747b38acefddfc99ad84ab7
itsvovova <itsvolodya@yandex.ru>
2025-08-14 13:25:06 +0300

    Updated main.go

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
-	"math"
 	"net/http"
 	"speechToText/src/cache"
 )
@@ -10,13 +9,8 @@ import (
 var SessionManager *cache.RedisSessionManager
 
 func main() {
-	var sessionProvider, err = cache.NewRedisSessionProvider(":8080")
-	if err != nil {
-		panic(err)
-	}
-	SessionManager = cache.NewRedisSessionManager("session_id", sessionProvider, int64(math.Pow10(5)))
 	var r = chi.NewRouter()
-	err = http.ListenAndServe(":8000", r)
+	err := http.ListenAndServe(":8000", r)
 	if err != nil {
 		panic(err)
 	}
