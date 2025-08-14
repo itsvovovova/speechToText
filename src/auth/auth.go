6bd4b1edfc3451d63e70c30cdb4f38f93cc3da6e
itsvovovova <itsvolodya@yandex.ru>
2025-08-14 11:18:26 +0300

    updated struct

--- src/auth/auth.go
+++ src/auth/auth.go
@@ -16,4 +16,57 @@ func register(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	var response = struct {
+		Result string `json:"result"`
+	}{
+		Result: "ok",
+	}
+	rvalue, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if _, err := w.Write(rvalue); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	user, err := service.ReadAuthRequest(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	exist, err := db.CheckAuthData(user.Username, user.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if !exist {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
+	ctx := r.Context()
+	session, err := cache.SessionManager.SessionStart(ctx, w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	data := struct {
+		Result string `json:"result"`
+		Token  string `json:"token"`
+	}{
+		Result: "ok",
+		Token:  session.SessionId,
+	}
+	rvalue, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if _, err := w.Write(rvalue); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
