3fa1da9b8cb53baded59fe5d660363bd8833f6ae
itsvovova <itsvolodya@yandex.ru>
2025-08-15 18:32:12 +0300

    Added: db connection, service

--- src/db/database.go
+++ src/db/database.go
@@ -1,1 +1,47 @@
 package db
+
+import (
+	"database/sql"
+	"log"
+)
+
+var db *sql.DB
+
+func InitDB() *sql.DB {
+	connStr := "host=localhost port=5432 user=youruser password=yourpassword dbname=yourdb sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(db)
+	if err := db.Ping(); err != nil {
+		log.Fatal("Не удалось подключиться к базе данных:", err)
+		return nil
+	}
+	var query1 = `
+    CREATE TABLE IF NOT EXISTS users (
+        username VARCHAR(1000) NOT NULL,
+        password VARCHAR(1000) NOT NULL,
+    );`
+	_, err = db.Exec(query1)
+	if err != nil {
+		log.Fatal("Ошибка при создании таблицы:", err)
+	}
+	var query2 = `
+    CREATE TABLE IF NOT EXISTS tasks (
+        username VARCHAR(1000) NOT NULL,
+        task VARCHAR(1000) NOT NULL,
+        status VARCHAR(1000) NOT NULL,
+        result VARCHAR(1000) NOT NULL,
+    );`
+	_, err = db.Exec(query2)
+	if err != nil {
+		log.Fatal("Ошибка при создании таблицы:", err)
+	}
+	return db
+}
--- src/db/service.go
+++ src/db/service.go
@@ -1,21 +1,48 @@
 package db
 
+import (
+	"log"
+)
+
 func AddAuthData(username string, password string) error {
+	query := "INSERT INTO users (username, password) VALUES ($1, $2)"
+	if _, err := db.Exec(query, username, password); err != nil {
+		return err
+	}
 	return nil
 }
 
 func CheckAuthData(username string, password string) (bool, error) {
-	return true, nil
+	var exists bool
+	err := db.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE username = $1 AND password = $2)", username, password).Scan(&exists)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return exists, nil
 }
 
 func AddAudioTask(username string, audio string) error {
+	query := "INSERT INTO audio (username, task,) VALUES ($1, $2)"
+	if _, err := db.Exec(query, username, audio); err != nil {
+		return err
+	}
 	return nil
 }
 
 func GetStatusTask(username string) (string, error) {
-	return "", nil
+	var status string
+	err := db.QueryRow("SELECT status FROM task WHERE username = $1", username).Scan(&status)
+	if err != nil {
+		return "", err
+	}
+	return status, nil
 }
 
 func GetResultTask(username string) (string, error) {
-	return "", nil
+	var status string
+	err := db.QueryRow("SELECT result FROM task WHERE username = $1", username).Scan(&status)
+	if err != nil {
+		return "", err
+	}
+	return status, nil
 }
