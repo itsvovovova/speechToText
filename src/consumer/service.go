f15cb8ec16fd1d0d886ba607e7ff89d050525b31
itsvovova <itsvolodya@yandex.ru>
2025-08-17 17:01:57 +0300

    Updated: consumer functions

--- src/consumer/service.go
+++ src/consumer/service.go
@@ -1,66 +1,157 @@
 package consumer
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
+	"speechToText/src/db"
 	"speechToText/src/types"
 )
-
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Panicf("%s: %s", msg, err)
-	}
-}
 
 func connectionAMQP(url string) *amqp.Connection {
 	connection, err := amqp.Dial(url)
 	if err != nil {
-		failOnError(err, "Failed to connect to RabbitMQ")
+		panic(err)
 	}
 	return connection
 }
 
-func SendMessage(taskID string, nameQueue string, audioUrl string) {
-	connection := connectionAMQP(audioUrl)
-	defer func(Connection *amqp.Connection) {
-		err := Connection.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(connection)
+func declareQueue(RabbitMQUrl string, nameQueue string) (*types.QueueRabbitMQ, error) {
+	connection := connectionAMQP(RabbitMQUrl)
 	channel, err := connection.Channel()
 	if err != nil {
-		log.Fatal(err)
-		return
+		_ = connection.Close()
+		return nil, err
 	}
-	defer func(channel *amqp.Channel) {
-		err := channel.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(channel)
 	queue, err := channel.QueueDeclare(nameQueue, false, false, false, false, nil)
 	if err != nil {
-		log.Fatal(err)
-		return
+		_ = connection.Close()
+		return nil, err
 	}
+	return &types.QueueRabbitMQ{
+		Queue:      &queue,
+		Channel:    channel,
+		Connection: connection,
+	}, nil
+}
+
+func SendMessage(taskID string, nameQueue string, audioUrl string, RabbitMQUrl string) error {
+	queueSettings, err := declareQueue(RabbitMQUrl, nameQueue)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err := closeQueueRabbitMQ(queueSettings)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
 	body := types.AudioMessage{
 		TaskID: taskID,
 		Audio:  audioUrl,
 	}
 	bodyJSON, err := json.Marshal(body)
 	if err != nil {
-		log.Fatal(err)
-		return
+		return err
 	}
-	err = channel.Publish(
+	err = queueSettings.Channel.Publish(
 		"",
-		queue.Name,
+		queueSettings.Queue.Name,
 		false,
 		false,
 		amqp.Publishing{
 			ContentType: "application/json",
 			Body:        bodyJSON,
 		})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ReceiveMessage(nameQueue string, ctx context.Context) error {
+	queueSettings, err := declareQueue(nameQueue, nameQueue)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err := closeQueueRabbitMQ(queueSettings)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
+	messages, err := queueSettings.Channel.ConsumeWithContext(
+		ctx,
+		nameQueue,
+		"",
+		false,
+		false,
+		true,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			select {
+			case message, ok := <-messages:
+				if !ok {
+					fmt.Println("Channel closed")
+					return
+				}
+				err := processingData(message)
+				if err != nil {
+					fmt.Println("error: ", err)
+					err = message.Nack(false, true)
+					if err != nil {
+						return
+					}
+				} else {
+					err := message.Ack(false)
+					if err != nil {
+						return
+					}
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-ctx.Done()
+	return nil
+}
+
+func processingData(data amqp.Delivery) error {
+	var audio types.AudioMessage
+	err := json.Unmarshal(data.Body, &audio)
+	if err != nil {
+		return err
+	}
+	text, err := ConvertToText(audio.Audio)
+	if err != nil {
+		return err
+	}
+	if err = db.AddResultTask(audio.TaskID, text); err != nil {
+		return err
+	}
+	return nil
+}
+
+func closeQueueRabbitMQ(queue *types.QueueRabbitMQ) error {
+	if queue.Channel != nil {
+		err := queue.Channel.Close()
+		if err != nil {
+			return err
+		}
+	}
+	if queue.Connection != nil {
+		return queue.Connection.Close()
+	}
+	return nil
 }
